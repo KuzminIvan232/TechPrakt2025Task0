df7182f9f3b50e2047f28c2582876d55c51d79e2
Кузьмін Іван Андрійович <vt232_kia@student.ztu.edu.ua>
2025-01-23 23:44:42 +0200

    Додано файл Програми

---
+++ Program/Task1/Project1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Console.Write("Введіть перше число: ");
+            int a = int.Parse(Console.ReadLine());
+
+            Console.Write("Введіть друге число: ");
+            int b = int.Parse(Console.ReadLine());
+
+            int sum = a + b;
+
+            Console.WriteLine("Сума: " + sum);
+
+            Console.WriteLine("\nНатисніть будь-яку клавішу, щоб вийти...");
+            Console.ReadKey();
+        }
+    }
+}
